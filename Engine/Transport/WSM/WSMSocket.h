1627ca8b416e2920446c644ec8b2ddd9e6674ce6
Anton Golovkov <udattsk@gmail.com>
2024-01-26 12:41:44 +0700

    disable traces

--- Engine/Transport/WSM/WSMSocket.h
+++ Engine/Transport/WSM/WSMSocket.h
@@ -30,7 +30,7 @@ public:
     /// ISocket impl
     virtual void Send(const IPacket &packet, const Address *address = nullptr) final;
 
-    static constexpr bool WITH_TRACES = true;
+    static constexpr bool WITH_TRACES = false;
 
 private:
     std::unique_ptr<WSMSocketImpl> impl;
