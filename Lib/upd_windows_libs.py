53dd1635932143c66594d74959b7a068e5d3420a
Anton Golovkov <udattsk@gmail.com>
2024-01-28 13:31:57 +0700

    upd lib

--- Lib/upd_windows_libs.py
+++ Lib/upd_windows_libs.py
@@ -22,7 +22,7 @@ web_m = "1Roe3DWRoR5jg1GqTsvpFSU_t3KsUabH9"
 
 open_ssl = "1U5Pm4iiJumWcDC03bLd6NY_ifdhCYB2m"
 
-wui = "1GXtOsge7Bis50Nc4rfDstKmahTAV9X6l"
+wui = "1aS-2gldf14JW6YnExoaGtszpVS4CgFfA"
 
 libs = { "boost":    boost,
          "db":       db,
