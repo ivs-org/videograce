85882d6525b5b437557b818d97412dabaff9f61c
Anton Golovkov <udattsk@gmail.com>
2024-02-12 16:49:48 +0700

    Improved deploy

--- Doc/Client/deploy.cmd
+++ Doc/Client/deploy.cmd
@@ -1,19 +1,19 @@
 cd en
-mkdocs build
-del /S /Q ..\..\..\sites\wui_site\doc
-xcopy /E /Y site ..\..\..\\sites\wui_site\doc
-del /S /Q site
-rmdir /S /Q site
-mkdir ..\..\..\sites\wui_site\doc\img
-copy img\* ..\..\..\sites\wui_site\doc\img
+rem mkdocs build
+rem del /S /Q ..\..\..\..\sites\vg_site\doc\client
+rem xcopy /E /Y site ..\..\..\..\sites\vg_site\doc\client
+rem del /S /Q site
+rem rmdir /S /Q site
+rem mkdir ..\..\..\..\sites\vg_site\doc\client\img
+rem copy img\* ..\..\..\..\sites\vg_site\doc\client\img
 
 cd ..\ru
 mkdocs build
-del /S /Q ..\..\..\sites\wui_site\doc_ru
-xcopy /E /Y site ..\..\..\sites\wui_site\doc_ru
+del /S /Q ..\..\..\..\sites\vg_site_ru\doc\client\
+xcopy /E /Y site ..\..\..\..\sites\vg_site_ru\doc\client\
 del /S /Q site
 rmdir /S /Q site
-mkdir ..\..\..\sites\wui_site\doc_ru\img
-copy img\* ..\..\..\sites\wui_site\doc_ru\img
+mkdir ..\..\..\..\sites\vg_site_ru\doc\client\img
+copy img\* ..\..\..\..\sites\vg_site_ru\doc\client\img
 
 pause
