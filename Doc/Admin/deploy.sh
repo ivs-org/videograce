ea28dad06ef4a703bdfe58ee9bfb21a07bd80f65
Anton Golovkov <udattsk@gmail.com>
2024-02-15 17:20:47 +0700

    upd

--- Doc/Admin/deploy.sh
+++ Doc/Admin/deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 cd en
-mkdocs build
-rm -rf ../../../wui_site/doc
-mv site ../../../wui_site/doc
-mkdir -p ../../../wui_site/doc/img
-cp img/* ../../../wui_site/doc/img
+#mkdocs build
+#rm -rf ../../../vg_site/doc/admin
+#mv site ../../../vg_site/doc/admin
+#mkdir -p ../../../vg_site/doc/admin/img
+#cp img/* ../../../vg_site/doc/admin/img
 
 cd ../ru
 mkdocs build
-rm -rf ../../../wui_site/doc_ru
-mv site ../../../wui_site/doc_ru
-mkdir -p ../../../wui_site/doc_ru/img
-cp img/* ../../../wui_site/doc_ru/img
+rm -rf ../../../vg_site_ru/doc/admin
+mv site ../../../vg_site_ru/doc/admin
+mkdir -p ../../../vg_site_ru/doc/admin/img
+cp img/* ../../../vg_site_ru/doc/admin/img
