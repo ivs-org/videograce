60cbf5aa357b588436dc47d7bbc5127f19a86c60
Anton Golovkov <udattsk@gmail.com>
2024-03-10 15:41:06 +0700

    Version inc

--- Engine/Version.h
+++ Engine/Version.h
@@ -20,9 +20,9 @@
 
 #define MAJOR_VERSION 2
 #define MINOR_VERSION 1
-#define RELEASE 24306
+#define RELEASE 24310
 
-#define CLIENT_VERSION 538
+#define CLIENT_VERSION 539
 #define SERVER_VERSION 316
 
 #define STR_HELPER(x) #x
